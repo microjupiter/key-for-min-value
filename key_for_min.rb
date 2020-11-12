e80be1e24046ba23029e12349908856bab2dcf05
Kyle Wilson <kyle@hinterlandstills.com>
2020-11-12 14:37:55 -0600

    commit tree c0011531c58f557a30b4592572b59a34b5099bcc

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,16 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
 
+
+def key_for_min_value(name_hash)
+    lowest_name = nil
+    lowest_num = nil
+    name_hash.each do |name, num|
+        if lowest_num == nil || num < lowest_num
+            lowest_num = num
+            lowest_name = name
+        end
+    end
+    lowest_name
 end
